6c4f48226284317b0bd4b62a373d6598065ff2b3
jbxamora <jbxamora@icloud.com>
2023-03-26 15:50:47 -0700

    updated create post page to include a supriseme prompt(not ready) 2 forms a placeholder and a some loaders. created empty handle functions

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-function FormField() {
+function FormField({ LabelName, type, name, placeholder, value, handleChange, isSupriseMe, handleSupriseMe }) {
   return (
     <div>
-      form
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+        htmlFor={name}
+        className='block text-sm font-medium text-gray-900'
+        >
+        {LabelName}
+        </label>
+        {isSupriseMe && (
+        <button
+        type="button"
+        onClick={handleSupriseMe}
+        className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+        >
+            Suprise Me
+        </button>
+        )}
+      </div>
+      <input 
+      type={type}
+      id={name}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+      className='bg-gray-50 border border-gray-350 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      />
     </div>
   )
 }
