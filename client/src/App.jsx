12f4a60576bb02378883b73d1335d9d1953bad12
jbxamora <jbxamora@icloud.com>
2023-03-26 14:06:19 -0700

    created client folder structer // added dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+
+import { logo } from './assets';
+
+
+const App = () => {
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Hello World
+    </h1>
+  )
+}
+
+export default App
