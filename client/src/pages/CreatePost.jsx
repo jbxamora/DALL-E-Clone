ec80584610ed2974d825909f9eaf18704848b1d3
jbxamora <jbxamora@icloud.com>
2023-03-27 20:08:27 -0700

    updated create page with new button meant to share with community once backend is integrated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,6 +17,10 @@ function CreatePost() {
     const [generatingImg, setGeneratingImg] = useState(false);
     const [loading, setLoading] = useState(false);
 
+    const generateImage = () => {
+
+    }
+
     const handleSubmit = () => {
 
     }
@@ -78,6 +82,30 @@ function CreatePost() {
                     <Loader />
                 </div>
             )}
+        </div>
+
+        <div className='mt-5 flex gap-5'>
+            <button 
+            type='button'
+            onClick={generateImage}
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text center'
+            >
+                {generatingImg ? 'Generating Image' : 'Generate Image'}
+            </button>
+        </div>
+
+        <div className='mt-10'>
+            <p className='mt-2 text-[#666e75] text-[14px]'>
+                Once you have created the image you want, you can share it with others in the community!
+            </p>
+            <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            >
+                {loading ? 'Sharing...' : 'Share With the Community'}
+
+            </button>
+
         </div>
       </form>
     </section>
