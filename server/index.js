60b4970fac0b4f676dc14675fa3158e274a6f697
jbxamora <jbxamora@icloud.com>
2023-03-28 02:42:29 -0700

    finished simple instance of api // started server that connects to mongodb // added postroutes/dalleroutes structure

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connection.js";
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from "./routes/dalleRoutes.js"
+
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
