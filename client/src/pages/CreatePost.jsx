81cd8b2e96907da3c7e9be6a9b15f359b1038937
jbxamora <jbxamora@icloud.com>
2023-03-29 22:28:49 -0700

    updated server to reflect live deployment on render

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "http://localhost:8080/api/v1/dalle",
+          "https://dallecloneserver-pcz4.onrender.com/api/v1/dalle",
           {
             method: "POST",
             headers: {
@@ -61,7 +61,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post",
+          "https://dallecloneserver-pcz4.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:8080/api/v1/post",
+        "https://dallecloneserver-pcz4.onrender.com/api/v1/post",
         {
           method: "GET",
           headers: {
