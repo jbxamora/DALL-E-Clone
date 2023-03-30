d7bb576c4751af681f4aed250920e4da908169ef
jbxamora <jbxamora@icloud.com>
2023-03-29 21:53:43 -0700

    formatted formfield, corrected development api routes // finished home page added search functionality

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React from "react";
 
-function FormField({ LabelName, type, name, placeholder, value, handleChange, isSupriseMe, handleSupriseMe }) {
-  return (
-    <div>
-      <div className='flex items-center gap-2 mb-2'>
-        <label
-        htmlFor={name}
-        className='block text-sm font-medium text-gray-900'
-        >
-        {LabelName}
-        </label>
-        {isSupriseMe && (
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => (
+  <div>
+    <div className="flex items-center gap-2 mb-2">
+      <label htmlFor={name} className="block text-sm font-medium text-gray-900">
+        {labelName}
+      </label>
+      {isSurpriseMe && (
         <button
-        type="button"
-        onClick={handleSupriseMe}
-        className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
         >
-            Suprise Me
+          Surprise me
         </button>
-        )}
-      </div>
-      <input 
+      )}
+    </div>
+    <input
       type={type}
       id={name}
       name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
       placeholder={placeholder}
       value={value}
       onChange={handleChange}
       required
-      className='bg-gray-50 border border-gray-350 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
-      />
-    </div>
-  )
-}
+    />
+  </div>
+);
 
-export default FormField
+export default FormField;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "http:localhost:8080/api/v1/dalle",
+          "http://localhost:8080/api/v1/dalle",
           {
             method: "POST",
             headers: {
@@ -61,7 +61,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http:localhost/api/v1/post",
+          "http://localhost:8080/api/v1/post",
           {
             method: "POST",
             headers: {
