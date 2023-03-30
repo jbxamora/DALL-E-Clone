0f03737f719d149ec2c38a25a429574afab69f31
jbxamora <jbxamora@icloud.com>
2023-03-29 21:09:48 -0700

    finished card layout // updated createpost page and homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,29 +20,6 @@ function Home() {
 
   const [searchText, setSearchText] = useState("");
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (response.ok) {
-          const result = await response.json();
-
-          setAllPosts(results.data.reverse());
-        }
-      } catch (error) {
-        alert(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-  }, []);
-
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -74,7 +51,7 @@ function Home() {
               {searchText ? (
                 <RenderCards data={[]} title="No results found" />
               ) : (
-                <RenderCards data={allPosts} title="No posts found" />
+                <RenderCards data={[]} title="No posts found" />
               )}
             </div>
           </>
